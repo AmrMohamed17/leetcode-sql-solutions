0346b184e85191c0bd171adbc2ace2e35896dce3
Amr Mohammed <abdelrahman.mohamed89@msa.edu.eg>
2025-11-25 09:15:51 +0200

    Medium

---
+++ Medium/1174-immediate-food-delivery-ii/solutions.sql
@@ -0,0 +1,13 @@
+SELECT 
+    ROUND(
+        AVG(d.order_date = d.customer_pref_delivery_date)*100,
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN (
+    SELECT customer_id, MIN(order_date) AS first_order
+    FROM Delivery
+    GROUP BY customer_id
+) t
+ON d.customer_id = t.customer_id
+AND d.order_date = t.first_order;
